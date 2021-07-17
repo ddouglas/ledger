0a4cbcff7ffdac047d69bd1524dc386595b9b544
David Douglas <david@onetwentyseven.dev>
2021-07-17 00:35:20 +0000

    --- Auto-Git Commit ---

--- internal/importer/webhook.go
+++ internal/importer/webhook.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/ddouglas/ledger/internal/gateway"
+	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jws"
+	"github.com/lestrrat-go/jwx/jwt"
 	"github.com/plaid/plaid-go/plaid"
 )
 
@@ -47,6 +48,7 @@ func (s *service) VerifyWebhookMessage(ctx context.Context, header http.Header, 
 	}
 
 	messageSignatures := parsed.Signatures()
+
 	// I can't remember what to do after this. I remember checking for length of the signatures
 	// Singature returns the JWT Headers, that will allow me to get the KID
 	// Once I get I KID, I can reach out ot Plaid to fethc the key,
@@ -54,7 +56,40 @@ func (s *service) VerifyWebhookMessage(ctx context.Context, header http.Header, 
 	// Once signature is verified I know that the has in the body of the JWT is safe to use
 	// SHA256 Hash the message parameter of this function and compare it with the SHA256 in
 	// the body of the JWT. If they equal return nil
-	spew.Dump(messageSignatures)
+	if len(messageSignatures) > 0 {
+		return fmt.Errorf("expected a single signature, got %d", len(messageSignatures))
+	}
+
+	signature := messageSignatures[0]
+	protectedHeaders := signature.ProtectedHeaders()
+	if protectedHeaders.Algorithm() != "ES256" {
+		return fmt.Errorf("expected algo of ES256, got %s", protectedHeaders.Algorithm())
+	}
+
+	keyID := protectedHeaders.KeyID()
+	if keyID == "" {
+		return fmt.Errorf("expected non empty keyID")
+	}
+
+	verificationKey, err := s.gateway.WebhookVerificationKey(ctx, keyID)
+	if err != nil {
+		return err
+	}
+
+	verificationKeyBytes, err := json.Marshal(verificationKey)
+	if err != nil {
+		return fmt.Errorf("failed to marshal key to be parsed by jwk lib: %w", err)
+	}
+
+	key, err := jwk.ParseKey(verificationKeyBytes)
+	if err != nil {
+		return fmt.Errorf("failed to parse verification key to jwk: %w", err)
+	}
+
+	set := jwk.NewSet()
+	set.Add(key)
+
+	parsedVerificationHeader := jwt.ParseString(verificationJWT, jwt.WithKeySet(set))
 
 	return nil
 
