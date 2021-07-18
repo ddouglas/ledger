362dfa69702a5744d4ab121320aa9133ef64d4c3
David Douglas <david@onetwentyseven.dev>
2021-07-18 00:11:19 +0000

    --- Auto-Git Commit ---

--- internal/mysql/account.go
+++ internal/mysql/account.go
@@ -48,8 +48,13 @@ func (r *accountRepository) Account(ctx context.Context, itemID string, accountI
 		return nil, errors.Wrapf(err, "[Account] ItemID: %s AccountID: %s", itemID, accountID)
 	}
 
-	var account = new(ledger.Account)
-	err = r.db.GetContext(ctx, account, query, args...)
+	row := r.db.QueryRowxContext(ctx, query, args...)
+	if row.Err() != nil {
+
+		return nil, errors.Wrapf(row.Err(), "[Account] ItemID: %s AccountID: %s", itemID, accountID)
+	}
+
+	account, err := scanAccountFromRow(row)
 
 	return account, errors.Wrapf(err, "[Account] ItemID: %s AccountID: %s", itemID, accountID)
 
@@ -183,6 +188,60 @@ func scanAccountFromRows(rows *sqlx.Rows) ([]*ledger.Account, error) {
 	}
 
 	return accounts, nil
+
+}
+
+func scanAccountFromRow(row *sqlx.Row) (*ledger.Account, error) {
+
+	var (
+		item_id                  string
+		account_id               string
+		mask                     null.String
+		name                     null.String
+		official_name            null.String
+		balance_available        float64
+		balance_current          float64
+		balance_limit            float64
+		balance_last_updated     null.Time
+		iso_currency_code        string
+		unofficial_currency_code null.String
+		subtype                  null.String
+		accountType              null.String
+		created_at               time.Time
+		updated_at               time.Time
+	)
+
+	err := row.Scan(
+		&item_id, &account_id, &mask, &name,
+		&official_name, &balance_available, &balance_current, &balance_limit,
+		&balance_last_updated, &iso_currency_code, &unofficial_currency_code, &subtype,
+		&accountType, &created_at, &updated_at,
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "[scanAccountFromRows]")
+	}
+
+	account := &ledger.Account{
+		ItemID:       item_id,
+		AccountID:    account_id,
+		Mask:         mask,
+		Name:         name,
+		OfficialName: official_name,
+		Subtype:      subtype,
+		Type:         accountType,
+		CreatedAt:    created_at,
+		UpdatedAt:    updated_at,
+		Balance: &ledger.AccountBalance{
+			Available:              balance_available,
+			Current:                balance_current,
+			Limit:                  balance_limit,
+			ISOCurrencyCode:        iso_currency_code,
+			UnofficialCurrencyCode: unofficial_currency_code,
+			LastUpdated:            balance_last_updated,
+		},
+	}
+
+	return account, nil
 
 }
 
