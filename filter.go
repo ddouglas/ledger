4b9eece84f40fab6135720ff69eefe6c618711bb
David Douglas <david@onetwentyseven.dev>
2021-07-18 20:11:53 +0000

    --- Auto-Git Commit ---

--- filter.go
+++ filter.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Masterminds/squirrel"
 	sq "github.com/Masterminds/squirrel"
 )
 
@@ -23,13 +22,13 @@ func NewTimeFilter(op Operation, t time.Time) (TimeFilter, error) {
 	}, nil
 }
 
-func (f TimeFilter) ToSqlizer() squirrel.Sqlizer {
-
+func (f TimeFilter) ToSql(column string) sq.Sqlizer {
+	return f.operation.ToSqlizer(column, f.time)
 }
 
 type StringFilter struct {
 	operation Operation
-	string    string
+	str       string
 }
 
 func NewStringFilter(op Operation, str string) (StringFilter, error) {
@@ -41,6 +40,10 @@ func NewStringFilter(op Operation, str string) (StringFilter, error) {
 		op, str,
 	}, nil
 
+}
+
+func (f StringFilter) ToSql(column string) sq.Sqlizer {
+	return f.operation.ToSqlizer(column, f.str)
 }
 
 type NumberFilter struct {
@@ -58,8 +61,27 @@ func NewNumberFilter(op Operation, number int64) (NumberFilter, error) {
 	}, nil
 }
 
+func (f NumberFilter) ToSql(column string) sq.Sqlizer {
+	return f.operation.ToSqlizer(column, f.number)
+}
+
 type OrderByFilter struct {
-	Direction Direction
+	column    string
+	direction Direction
+}
+
+func NewOrderByFilter(d Direction, c string) (OrderByFilter, error) {
+	if !d.IsValid() {
+		return OrderByFilter{}, fmt.Errorf("%s is not a valid order by direction", string(d))
+	}
+
+	return OrderByFilter{
+		c, d,
+	}, nil
+}
+
+func (o OrderByFilter) ToSql() sq.Sqlizer {
+
 }
 
 type Operation string
@@ -115,3 +137,15 @@ const (
 	AscDirection  Direction = "ASC"
 	DescDirection Direction = "DESC"
 )
+
+var AllDirections = []Direction{AscDirection, DescDirection}
+
+func (d Direction) IsValid() bool {
+	for _, dir := range AllDirections {
+		if d == dir {
+			return true
+		}
+	}
+
+	return false
+}
